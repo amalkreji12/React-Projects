e485455b60c6436d0b2315b60efb1cbb0fa657e2
Amal K Reji <amalreji45.amal@gmail.com>
2025-01-07 13:20:58 +0530

    firebase connected

--- meet-up/src/Pages/NewMeetUp.js
+++ meet-up/src/Pages/NewMeetUp.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import NewMeetUpForm from '../components/MeetUps/NewMeetUpForm'
+import { useNavigate } from 'react-router-dom'
 
 const NewMeetUp = () => {
+  const navigate = useNavigate();
+
   function addMeetUpHandler(meetupData) {
-    fetch('https://react-meetup-7c7a2-default-rtdb.firebaseio.com/meetups.json');
+    fetch('https://react-meetup-7c7a2-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      navigate('/');
+    });
   }
   return (
     <div>
