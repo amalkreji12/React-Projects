8d8af515f73f665f8a9d9a15902236657faf7e47
Amal K Reji <amalreji45.amal@gmail.com>
2025-01-14 15:35:08 +0530

    api added

--- movielist/src/App.js
+++ movielist/src/App.js
@@ -1,9 +1,52 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from './Search.svg';
+import MovieCard from './components/MovieCard';
+
+const apiKey = 'http://www.omdbapi.com/?apikey=f176d329'
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${apiKey}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovie('Harry Potter');
+  }, []);
+
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <h1>Movie List</h1>
+
+      <div className='search'>
+        <input placeholder='Search for movies' value='movie' onChange={() => { }} />
+        <img src={SearchIcon} alt='Search Icon' onClick={() => { }} />
+      </div>
+
+      {
+        movies?.length > 0 ?
+          (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard key={movie.imdbID} movies={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found ! </h2>
+            </div>
+          )
+      }
+
+
     </div>
   );
 }
