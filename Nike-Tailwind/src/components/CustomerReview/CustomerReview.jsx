a98d31e3ad5221f217a558f4dd808a5a7f7d1d6e
Amal K Reji <amalreji45.amal@gmail.com>
2025-01-20 15:21:53 +0530

    Review

--- Nike-Tailwind/src/components/CustomerReview/CustomerReview.jsx
+++ Nike-Tailwind/src/components/CustomerReview/CustomerReview.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { reviews } from "../../constants";
+import { ReviewCard } from "../ReviewCard/ReviewCard";
 
 const CustomerReview = () => {
   return (
-    <div>CustomerReview</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin font-bold text-center text-4xl">
+        What Our <span className="text-coral-red">Customer</span> Say ?{" "}
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia
+        nesciunt iusto eos rem, ipsam illo vitae unde.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default CustomerReview
+export default CustomerReview;
